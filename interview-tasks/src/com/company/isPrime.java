459a7b53278119a159a0773b93e476006ef6593b
jacek <jaco04@wp.pl>
2019-01-15 20:36:16 +0100

    isPrime

---
+++ interview-tasks/src/com/company/isPrime.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class isPrime {
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
